6a236d3f9150ef8c8ec870601812883d255e3459
kennedypbrown <kennedypbrown@berkeley.edu>
2026-05-12 11:23:04 -0700

    Update 1-waterpoint_image_compilation.R

--- 1-RCT-design/1-waterpoint_image_compilation.R
+++ 1-RCT-design/1-waterpoint_image_compilation.R
@@ -22,105 +22,113 @@ dta <- read_excel(clean_merged_dta) %>%
   mutate(
     water_point_picture = coalesce(water_point_picture.x, water_point_picture.y)
   ) %>% 
-  select(wp_id_new, state, ward, community, water_point_picture, -water_point_picture.x, -water_point_picture.y)
+  select(wp_id_new, state, ward, community, enumerator, water_pt_type, water_point_picture, -water_point_picture.x, -water_point_picture.y)
 
-output<- file.path(clean, "Media")
+# ----------------------------------
+# WATER POINT IMAGE SUMMARY
+# ----------------------------------
 
-#Create a media folder
-if (!dir.exists(output)) {
-  dir.create(output, recursive = TRUE)}
-
-# Loop over each row and file into state/ward/community/type folders
-walk(1:nrow(dta), function(i) {
-  row <- dta[i, ]
-  
-  # Build nested folder path
-  folder <- file.path(
-    output,
-    row$state,
-    row$ward,
-    row$community
+wp_image_state <- dta %>% 
+  group_by(state) %>% 
+  summarise(
+    missing_images = sum(is.na(water_point_picture)),
+    image_present = sum(!is.na(water_point_picture)),
+    total_waterpoints = n()
   )
-  
-  # Create folder if it doesn't exist
-  if (!dir.exists(folder)) dir.create(folder, recursive = TRUE)
-  
-  # Strip leading "media/" or "media\" at the start
-  photo_file <- sub("^media[\\\\/]", "", row$water_point_picture)
-  
-  # Skip if no image
-  if (is.na(photo_file) || photo_file == "") return(NULL)
 
-    
-  
-  src_1 <- file.path(scoping_emedia, photo_file)
-  src_2 <- file.path(scoping_nemedia, photo_file)
-  
-  #Combine both file sources
-  src <- if (file.exists(src_1)) {
-    src_1
-  } else if (file.exists(src_2)) {
-    src_2
-  } else {
-    warning(paste("File not found in either source:", row$water_point_picture))
-    return(NULL)
-  }
-  
-  
-  # Rename to wp_id_new, preserving original file extension
-  ext <- tools::file_ext(photo_file)
-  new_filename <- paste0(row$wp_id_new, ".", ext)
-  dst <- file.path(folder, new_filename)
-   
-  # Copy if source file exists
-  file.copy(src, dst, overwrite = TRUE)
-})
+wp_image_ward <- dta %>% 
+  group_by(ward) %>% 
+  summarise(
+    missing_images = sum(is.na(water_point_picture)),
+    image_present = sum(!is.na(water_point_picture)),
+    total_waterpoints = n()
+  )
 
+wp_image_community <- dta %>% 
+  group_by(community) %>% 
+  summarise(
+    missing_images = sum(is.na(water_point_picture)),
+    image_present = sum(!is.na(water_point_picture)),
+    total_waterpoints = n()
+  )
 
+wp_image_enumerator <- dta %>% 
+  group_by(enumerator) %>% 
+  summarise(
+    missing_images = sum(is.na(water_point_picture)),
+    image_present = sum(!is.na(water_point_picture)),
+    total_waterpoints = n()
+  )
 
+# wp_type_ward <- dta %>% 
+#   group_by(ward) %>% 
+#   summarise( 
+#     HP = sum(water_pt_type == "HP"),
+#     MB = 
+      
+  wp_type_ward <- dta %>% 
+  count(community, water_pt_type) %>% 
+  pivot_wider(names_from = water_pt_type, values_from = n, values_fill = 0)
 
+# I already ran this and just want to comment it out for now
+# It takes about 30-45 minutes to run, if needed to recompile we can 
+# un comment this
 
-
-# #Name of image column
-# photo_cols <- dta %>% select(water_point_picture)
-# #Writing code to organize the images by the state, community, and WP type
-# dta_state <- dta %>% 
-#   group_by(state) 
+# ----------------------------------
+# MEDIA FILE ORGANIZATION
+# ----------------------------------
+# output<- file.path(clean, "Media")
 # 
-#  df_name <- deparse(substitute(df))
-#   folder_path <- file.path(base_path, df_name)
-#   dir_create(folder_path)
+# #Create a media folder
+# if (!dir.exists(output)) {
+#   dir.create(output, recursive = TRUE)}
+# 
+# # Loop over each row and file into state/ward/community/type folders
+# walk(1:nrow(dta), function(i) {
+#   row <- dta[i, ]
+#   
+#   # Build nested folder path
+#   folder <- file.path(
+#     output,
+#     row$state,
+#     row$ward,
+#     row$community
+#   )
+#   
+#   # Create folder if it doesn't exist
+#   if (!dir.exists(folder)) dir.create(folder, recursive = TRUE)
+#   
+#   # Strip leading "media/" or "media\" at the start
+#   photo_file <- sub("^media[\\\\/]", "", row$water_point_picture)
+#   
+#   # Skip if no image
+#   if (is.na(photo_file) || photo_file == "") return(NULL)
+# 
+#     
+#   
+#   src_1 <- file.path(scoping_emedia, photo_file)
+#   src_2 <- file.path(scoping_nemedia, photo_file)
+#   
+#   #Combine both file sources
+#   src <- if (file.exists(src_1)) {
+#     src_1
+#   } else if (file.exists(src_2)) {
+#     src_2
+#   } else {
+#     warning(paste("File not found in either source:", row$water_point_picture))
+#     return(NULL)
+#   }
 #   
 #   
-#   for (i in seq_len(nrow(df))) {
-#     for (col in photo_cols) {
-#       photo_file <- df[[col]][i]
-#       if (!is.na(photo_file) && photo_file != "") {
-#         local_photo <- file.path(media_folder, sub("^media[\\\\/]", "", photo_file))
-#         
-#         if (file_exists(local_photo)) {
-#           safe_name <- str_replace_all(paste(
-#             df$facility_name[i],
-#             df$facility_id[i],
-#             col, i,
-#             basename(local_photo)), "[^A-Za-z0-9_\\.]", "_")
-#           
-#           file_copy(local_photo, file.path(folder_path, safe_name), overwrite = TRUE)
-#         } else {
-#           if (grepl("media", photo_file, ignore.case = TRUE)) {
-#             # Add to the temporary list
-#             current_missing_list[[length(current_missing_list) + 1]] <- data.frame(
-#               log_type = df_name, # Added this so you know which log it came from!
-#               row_index = i,
-#               facility_id = df$facility_id[i],
-#               facility_name = df$facility_name[i],
-#               upload_date = df$upload_date[i],
-#               column_name = col,
-#               expected_path = local_photo,
-#               stringsAsFactors = FALSE)}
-#           warning("File does not exist: ", local_photo)
-#         }}}}
-#   
-#   
-#   # Report findings
-#   message("Photos for ", df_name, " saved to: ", folder_path)
+#   # Rename to wp_id_new, preserving original file extension
+#   ext <- tools::file_ext(photo_file)
+#   new_filename <- paste0(row$wp_id_new, ".", ext)
+#   dst <- file.path(folder, new_filename)
+#    
+#   # Copy if source file exists
+#   file.copy(src, dst, overwrite = TRUE)
+# })
+
+
+
+
