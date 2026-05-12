5802e706feb4cc5a8355f7d484fbae738d81ad57
kennedypbrown <kennedypbrown@berkeley.edu>
2026-05-12 14:46:15 -0700

    Update waterpoint_IDs.R
    Update new ID format to remove dash

--- 1-RCT-design/waterpoint_IDs.R
+++ 1-RCT-design/waterpoint_IDs.R
@@ -16,7 +16,7 @@ encrypt <- read_excel(scoping_edta)
 dta <- read_dta(old_merged_dta)
 
 dta<- dta %>% 
-  mutate(wp_id_type = paste0(state,community, "-", water_pt_type,"-")) %>% 
+  mutate(wp_id_type = paste0(state,community, "-", water_pt_type)) %>% 
   group_by(community) %>% 
   mutate(idx = row_number(),
          wp_id_new= paste0(wp_id_type, idx)) %>% 
